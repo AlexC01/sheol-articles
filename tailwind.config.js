3d334231ee9b675e816e3aa53e7b6e2f125f7acb
AlexC01 <acuadraq@gmail.com>
2023-07-21 16:16:30 -0600

    feat: adding new dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: "#2af73b",
+
+					secondary: "#bf8601",
+
+					accent: "#5fd3dd",
+
+					neutral: "#211c27",
+
+					"base-100": "#2c2f44",
+
+					info: "#4967d4",
+
+					success: "#1ec28c",
+
+					warning: "#d28f14",
+
+					error: "#ea4b48",
+				},
+			},
+		],
+	},
+};
