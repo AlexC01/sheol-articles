cc1c55d4276f145a31aee2dc64a156641ddb362d
AlexC01 <acuadraq@gmail.com>
2023-07-24 13:27:10 -0600

    style(tailwind-config): Themes
    Adding new color themes to the palet

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,30 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
-  plugins: [require("daisyui")]
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#cc394a",
+
+          secondary: "#97a3ed",
+
+          accent: "#edc71e",
+
+          neutral: "#252e37",
+
+          "base-100": "#30324b",
+
+          info: "#83a5dd",
+
+          success: "#2db9a1",
+
+          warning: "#f9920b",
+
+          error: "#f9544e"
+        }
+      }
+    ]
+  }
 };
