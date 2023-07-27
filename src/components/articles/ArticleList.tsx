e87169a58c176492aaae1195d3835c122172952b
AlexC01 <acuadraq@gmail.com>
2023-07-26 20:46:30 -0600

    feat(articles-list): fetching list
    Fetching list and creating empty component to be reusable

--- src/components/articles/ArticleList.tsx
+++ src/components/articles/ArticleList.tsx
@@ -1,13 +1,24 @@
+import getArticles from "@/actions/getArticles";
+import EmptyState from "../EmptyState";
 import ArticleCard from "../cards/ArticleCard";
 
-const ArticleList = () => {
+const ArticleList = async () => {
+  const articles = await getArticles();
+
+  if (articles.length === 0) {
+    return <EmptyState />;
+  }
   return (
-    <div className="mt-10 grid grid-cols-1 px-16 md:px-5 lg:px-0 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 sm:container mx-auto gap-10">
-      <ArticleCard />
-      <ArticleCard />
-      <ArticleCard />
-      <ArticleCard />
-      <ArticleCard />
+    <div className="mt-10 grid grid-cols-1 px-5 sosmall:12 md:px-5 lg:px-0 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 sm:container mx-auto gap-10">
+      {articles.map(article => (
+        <ArticleCard
+          key={article.id}
+          title={article.title}
+          content={article.content}
+          author={article.author.name}
+          image={article.image ?? ""}
+        />
+      ))}
     </div>
   );
 };
--- src/components/cards/ArticleCard.tsx
+++ src/components/cards/ArticleCard.tsx
@@ -1,28 +1,29 @@
 import Image from "next/image";
 
-const ArticleCard = () => {
+interface ArticleCardProps {
+  title: string;
+  date?: string;
+  content: string;
+  author: string;
+  image: string;
+}
+
+const ArticleCard: React.FC<ArticleCardProps> = ({ title, date, content, author, image }) => {
   return (
-    <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg border-black border-[1px]">
-      <Image
-        alt="Article Image"
-        className="h-56 w-full object-cover"
-        width={450}
-        height={450}
-        src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-      />
-      <div className="bg-white p-4 sm:p-6">
-        <time dateTime="2022-10-10" className="block text-xs text-gray-500">
-          10th Oct 2022
-        </time>
+    <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg border-black border-[1px] ">
+      <Image alt="Article Image" className="h-56 w-full object-cover" width={450} height={450} src={image} />
+      <div className="bg-white p-4 sm:p-6 ">
+        <div className="flex justify-between">
+          <span className="block text-xs text-gray-500 font-semibold">{author}</span>
+
+          <time dateTime="2022-10-10" className="block text-xs text-gray-500">
+            10th Oct 2022
+          </time>
+        </div>
         <a>
-          <h3 className="mt-0.5 text-lg text-gray-900">How to make a good pasta alfredo</h3>
+          <h3 className="mt-2 text-lg text-gray-900">{title}</h3>
         </a>
-        <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus pariatur animi
-          temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
-          voluptates neque explicabo tempora nisi culpa eius atque dignissimos. Molestias explicabo corporis voluptatem?
-        </p>
-        <span className="block text-xs text-gray-500 mt-3">Alex Cuadra</span>
+        <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 mb-2">{content}</p>
       </div>
     </article>
   );
