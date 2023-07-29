2647da2866e23a122174fd5a3675b025ffc7bdd4
AlexC01 <acuadraq@gmail.com>
2023-07-28 19:09:35 -0600

    feat(api-route): reviews endpoint
    Adding endpoint to post comments

---
+++ src/app/api/review/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/actions/getCurrentUser";
+import { prisma } from "@/lib/db/prisma";
+import { getArticleById } from "@/actions/getArticleById";
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return NextResponse.error();
+
+  const body = await request.json();
+  const { articleId, content } = body;
+
+  console.log(body);
+
+  const article = await getArticleById({ articleId });
+
+  if (!article) return NextResponse.error();
+
+  const comment = await prisma.review.create({
+    data: {
+      content,
+      authorId: currentUser.id,
+      articleId
+    }
+  });
+
+  return NextResponse.json(comment);
+}
